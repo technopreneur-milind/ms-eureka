ad1b00c1f771e83f0b8b006bc6e0bad4f96cc870
milind.technopreneur@gmail.com <milind.technopreneur@gmail.com>
2019-01-14 03:27:00 +0530

    first commit

---
+++ src/main/java/com/microservices/demo/DemoEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.microservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaApplication.class, args);
+	}
+
+}
+
